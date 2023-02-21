434d2f9c733da6d8c915d4007ee211ff850b9227
Abdulamer <mohammadabdul.amer@kanrad.com>
2023-02-21 10:08:32 +0530

    Added scroll container + APi call to get data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
+import Scroll from "./Scroll";
 import SearchBox from "./SearchBox";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   render() {
     const filteredRobots = this.state.robots.filter((robot) =>
       robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading..</h1>;
+    }
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox onSearchChange={this.onSearch}></SearchBox>
-        <CardList items={filteredRobots}></CardList>
+        <SearchBox onSearchChange={this.onSearch} />
+        <Scroll>
+          <CardList items={filteredRobots}></CardList>
+        </Scroll>
       </div>
     );
   }
